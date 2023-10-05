9b94e606a406d191f901df649386eefec9c6e2a0
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-10-05 16:25:17 -0300

    finished a few tables

--- transform_and_load.py
+++ transform_and_load.py
@@ -2,38 +2,28 @@ from sqlalchemy import text, create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.types import *
 import pandas as pd
-import os, json
-
-FILES_DIR = "Files"
-FILES_LIST = os.listdir(FILES_DIR)
-
-with open("all_dtypes.json", "r") as f:
-    all_dtypes = json.load(f)
-with open("file_table.json", "r") as f:
-    file_table = json.load(f)
+import os
 
 def get_engine_database(driver:str, username:str, password:str, host:str, port:str, database:str):
     """Create and return connection engine to DataBase"""
     return create_engine(URL.create(drivername=driver, username=username, password=password, host=host, port=port, database=database))
 
-def get_table_name(file_name:str) -> str:
-    return file_table[file_name.split(".")[0]]
-
-def get_dtypes(file_name:str) -> dict[str]:
-    return all_dtypes[get_table_name(file_name)]
-
-def get_columns(file_name:str) -> list[str]:
-    return [i for i, _ in get_dtypes(file_name).items()]
-
 with open("./secrets.txt", "r") as f:
     engine = get_engine_database(*f.read().split(","))
 
-file_name = FILES_LIST[0]
-cnaes = pd.read_csv(os.path.join(FILES_DIR,file_name), header=None, encoding="latin-1", sep=";", dtype=get_dtypes(file_name))
-cnaes.columns = get_columns(file_name)
+def create_insert(file_name:str, table_name:str, pk:str) -> None:
+    print(f"Creating and inserting on table {table_name}...")
+    df = pd.read_csv(os.path.join("Files",file_name), header=None, encoding="latin-1", sep=";", dtype={0:int,1:str})
+    df.columns = (pk, "descricao")
+    len_descricao = int(df["descricao"].str.len().max()*1.2)
+    with engine.connect() as conn:
+        df.to_sql(name=table_name, con=conn, if_exists="replace", index=False, dtype={pk:Integer(), "descricao":VARCHAR(len_descricao)})
+        conn.execute(text(f"ALTER TABLE public.{table_name} ADD PRIMARY KEY ({pk});"))
+        conn.commit()
 
-with engine.connect() as conn:
-    cnaes.to_sql(name=get_table_name(file_name), con=conn, if_exists="replace", index=False, dtype={"cnae_fiscal_principal":Integer(),
-                                                                                                    "descricao":VARCHAR(int(cnaes["descricao"].str.len().max()*1.2))})
-    conn.execute(text(f"ALTER TABLE public.id_cnae_fiscal_principal ADD PRIMARY KEY (cnae_fiscal_principal);"))
-    conn.commit()
+create_insert(file_name="Cnaes.csv", table_name="id_cnae", pk="cnae")
+create_insert(file_name="Motivos.csv", table_name="id_motivo_situacao_cadastral", pk="motivo_situacao_cadastral")
+create_insert(file_name="Municipios.csv", table_name="id_municipio", pk="municipio")
+create_insert(file_name="Naturezas.csv", table_name="id_natureza_juridica", pk="natureza_juridica")
+create_insert(file_name="Paises.csv", table_name="id_pais", pk="pais")
+create_insert(file_name="Qualificacoes.csv", table_name="id_qualificacao", pk="qualificacao")
