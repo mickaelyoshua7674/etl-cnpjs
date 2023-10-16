a98d8f963b49524fbb0b8c0ccaf90de80e54f5e1
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-10-16 19:38:44 -0300

    create requirements.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from subprocess import call
 from sys import executable
 call([executable,"-m","pip","install","-r","requirements.txt"])
+
+from dotenv import load_dotenv
+load_dotenv()
