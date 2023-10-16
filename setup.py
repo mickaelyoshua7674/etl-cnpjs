bf31f4d9f336ca54d8fa2f5d6064be8e68ef0005
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-10-16 19:40:38 -0300

    comments

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from subprocess import call
 from sys import executable
+# install all requirements
 call([executable,"-m","pip","install","-r","requirements.txt"])
 
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv() # creatre the environment variable in .env file
