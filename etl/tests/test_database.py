087a0c47d95263bf3f5ee4d6446935e38a0118e9
mickaelyoshua7674 <mickaelyoshua@gmail.com>
2023-11-22 16:56:25 -0300

    remove more dependency

--- etl/tests/test_database.py
+++ etl/tests/test_database.py
@@ -3,19 +3,16 @@ from sqlalchemy import text
 from os import environ
 import pytest
 
-@pytest.mark.dependency()
 def test_connection(my_engine) -> None:
     with my_engine.connect() as conn:
         res = conn.execute(text("SELECT 1;")).fetchone()[0]
         assert res == 1, "Result of query 'SELECT 1;' was different of 1"
 
-@pytest.mark.dependency(depends=["test_connection"])
 def test_database_exists(my_engine) -> None:
     with my_engine.connect() as conn:
         res = conn.execute(text(f"SELECT datname FROM pg_database WHERE datname='{environ["DB_NAME"]}';")).fetchone()[0]
         assert res == environ["DB_NAME"], "Database not found"
 
-@pytest.mark.dependency(depends=["test_database_exists"])
 def test_create_insert_aditional_tables(my_engine):
     data = (1, "desc")
     pk = "c3"
